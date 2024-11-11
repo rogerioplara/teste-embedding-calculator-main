fdc51ca5f946c8d3d1bfbb6ffdf38108e3a36c91
rogerioplara <rogerioo.lara@hotmail.com>
2024-11-11 17:36:52 -0300

    Atualização para es6

--- test_async.js
+++ test_async.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
-const path = require('path');
-const fetch = require('node-fetch'); // Continue usando require
-const FormData = require('form-data');
+import fs from 'fs';
+import path from 'path';
+import fetch from 'node-fetch';
+import FormData from 'form-data';
 
 const url = "http://localhost:3393/find_faces";
 const sampleImages = "./sample_images";
@@ -23,46 +23,32 @@ let iterationCount = 0;
 
 async function sendImage(imageName, imageData) {
     const formData = new FormData();
+    
     formData.append('image', Buffer.from(imageData), { filename: imageName, contentType: 'image/jpeg' });
 
     try {
         const startTime = Date.now();
-        const response = await fetch(`${url}`, {
+
+        const response = await fetch(url, {
             method: 'POST',
-            body: formData
+            body: formData,
+            headers: formData.getHeaders()
         });
 
         const responseData = await response.json();
         const result = responseData.faces || [];
         const numObjects = result.length;
-        
 
         const elapsedTime = Date.now() - startTime;
 
         console.log(`Img: ${imageName}\nStatus Code: ${response.status}, Tempo decorrido: ${elapsedTime}ms Iteration: ${iterationCount} | Subjects: ${numObjects}`);
 
-        // responseData.faces.forEach(face => {
-        //     const { age, detection, face_points, is_male, pose } = face;
-        //     const { bbox, det_score } = detection;
-        //     const { pitch, yaw, roll } = pose;
-
-        //     // Exibindo as informações no console
-        //     console.log(`Img: ${imageName}, Status Code: ${response.status}`);
-        //     console.log(`Idade: ${age}`);
-        //     console.log(`Detecção: ${JSON.stringify(bbox)} (Score: ${det_score})`);
-        //     console.log(`Pontos Faciais: ${JSON.stringify(face_points)}`);
-        //     console.log(`Gênero: ${is_male === 1 ? 'Masculino' : 'Feminino'}`);
-        //     console.log(`Pose (Pitch: ${pitch}, Yaw: ${yaw}, Roll: ${roll})`);
-        //     console.log('-------------------------------------------');
-        // });
-
         // console.log('Resposta completa do servidor:', JSON.stringify(responseData, null, 2));
-        
+
     } catch (e) {
         console.log(`Erro ao enviar a imagem ${imageName}: ${e.message}`);
     }
 }
-
 
 async function runTests() {
     while (true) {
